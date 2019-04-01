edf935364465d3fa03964dfffc274154c571b44e
dastanmazhitov <dastan.majitov1@gmail.com>
2019-04-01 19:41:45 +0600

    Add users models, serializers and views, add urls for authentification

--- crm/admin.py
+++ crm/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from users.models import Role
+from crm.models import *
 
-# Register your models here.
+
+admin.site.register(Tables)
+admin.site.register(Role)
+admin.site(Departments)
+admin.site(Meal_Categories)
+admin.site(Statuses)
+admin.site(Meals)
+admin.site(ServicePercentage)
+admin.site(Orders)
+admin.site(MealsToOrders)
+admin.site(Checks)
