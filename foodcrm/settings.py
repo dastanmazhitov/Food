e4d6b0385360a1d0fa23f60c2927d2ff7ee2c103
dastanmazhitov <dastan.majitov1@gmail.com>
2019-04-10 00:12:20 +0600

    Fix and add postgres for Docker

--- foodcrm/settings.py
+++ foodcrm/settings.py
@@ -76,20 +76,30 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodcrm.wsgi.application'
 
-
+DOCKER=True
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': '',
+if DOCKER:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'HOST': 'db',
+            'PORT': 5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': 5432,
+        }
+    }
 
 
 # Password validation
