03780b738c6d09cb58bce60e42d3ec4f431ac93c
dastanmazhitov <dastan.majitov1@gmail.com>
2019-04-09 15:29:00 +0600

    Fix admin models

--- crm/admin.py
+++ crm/admin.py
@@ -2,14 +2,13 @@ from django.contrib import admin
 from users.models import Role
 from crm.models import *
 
-
 admin.site.register(Tables)
 admin.site.register(Role)
-admin.site(Departments)
-admin.site(Meal_Categories)
-admin.site(Statuses)
-admin.site(Meals)
-admin.site(ServicePercentage)
-admin.site(Orders)
-admin.site(MealsToOrders)
-admin.site(Checks)
+admin.site.register(Departments)
+admin.site.register(Meal_Categories)
+admin.site.register(Statuses)
+admin.site.register(Meals)
+admin.site.register(ServicePercentage)
+admin.site.register(Orders)
+admin.site.register(MealsToOrders)
+admin.site.register(Checks)
